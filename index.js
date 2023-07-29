4851c493a1c59904bf97cd450dddd6cedb4cc2bb
Zaw Win Naung <amerizaw@gmail.com>
2023-07-29 12:35:35 +0630

    basket

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-ScrollReveal({
-  // reset: true,
-  distance: "80px",
-  duration: 2000,
-  delay: 200,
-});
-
-ScrollReveal().reveal(".intro_container", { origin: "top" });
-ScrollReveal().reveal(".nav_menu", { origin: "right" });
-ScrollReveal().reveal(".nav_bar", { origin: "top" });
-
 let images = ["img_slide_1.jpg", "img_slide_2.jpg", "img_slide_3.jpg"];
 let i = 0;
 const changeImage = () => {
@@ -20,4 +9,27 @@ const changeImage = () => {
     "linear-gradient(to bottom,rgba(13, 17, 23, 0.3),rgba(13, 17, 23, 1)),url(assets/images/" +
     images[i] +
     ")";
+};
+
+const addToBasket = (btn) => {
+  let item = btn.parentNode;
+  let children = item.children;
+  let name = children[0];
+  let price = children[1];
+  let str = "<div class='basket'>";
+  str += name + price;
+  str += "</div>";
+
+  if (sessionStorage.getItem("basket") == null) {
+    sessionStorage.setItem("basket", str);
+  } else {
+    let prev = sessionStorage.getItem("basket");
+    prev += str;
+    sessionStorage.setItem("basket", prev);
+    alert(sessionStorage.getItem("basket"));
+  }
+};
+
+const showData = () => {
+  basketData.innerHTML = sessionStorage.getItem("basket");
 };
---
+++ scroll_reveal.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".intro_container", { origin: "top" });
+ScrollReveal().reveal(".nav_bar", { origin: "top" });
